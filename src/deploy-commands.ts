5ab3754f508a50c2df0f141572b6c4ea3330b574
jayman <jayman.mcallister@gmail.com>
2024-02-15 15:53:02 -0800

    Update deploy-commands and package.json

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -2,7 +2,7 @@ import { REST, Routes } from "discord.js";
 import { config } from "./config";
 import { commands } from "./commands";
 
-const commandsData = Object.values(commands).map((command) => command.data1);
+const commandsData = Object.values(commands).map((command) => command.data);
 
 const rest = new REST({ version: "10" }).setToken(config.DISCORD_TOKEN);
 
