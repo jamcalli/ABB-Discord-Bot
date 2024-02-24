8037f94503e3efbafbcfc98d3321071db8381b63
jayman <jayman.mcallister@gmail.com>
2024-02-24 14:15:55 -0800

    Update .dockerignore and .gitignore files

--- playground/tst.ts
+++
@@ -1,8 +0,0 @@
-import { explore } from "../src/index";
-
-async function fetchAudiobooks() {
-  const audiobooks = await explore("category", "postapocalyptic", 2);
-  console.log(audiobooks);
-}
-
-fetchAudiobooks();
